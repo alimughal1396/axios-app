872ed7de66e79309ff6a7cde5f990353b70462c9
Ali Raza <aliraxa1396@gmail.com>
2023-05-19 02:07:04 +0500

    remove imports from App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import './App.css';
 import Routes from './Routing';
-// import Dashboard from './Dashboard';
-// import UserList from './UserList';
-// import PostList from './PostList';
 
 function App() {
   return (
     <>
       <Routes />
-      {/* <Dashboard />
-      <UserList />
-      <PostList /> */}
     </>
   );
 }
